79a9855db95b98af4e4d732c5d87cc2bf8bea35c
Peter Keating <peter@moov2.com>
2018-10-30 10:04:48 +0000

    Create empty module with manifest

---
+++ Manifest.cs
@@ -0,0 +1,8 @@
+using OrchardCore.Modules.Manifest;
+
+[assembly: Module(
+    Name = "SEO",
+    Author = "Moov2",
+    Website = "https://moov2.com",
+    Version = "0.0.1"
+)]
---
+++ Startup.cs
@@ -0,0 +1,13 @@
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using OrchardCore.Modules;
+
+namespace Moov2.OrchardCore.SEO
+{
+    public class Startup : StartupBase
+    {
+
+    }
+}
