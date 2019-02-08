6b507533b6bc34c55503ffc09065c1ca37f4efac
Peter Keating <peter@moov2.com>
2019-02-08 14:14:38 +0000

    Add MetaTagsPart under MetaTags feature
    Introduced a new feature for managing meta tags on a content item named
    "MetaTags". Enabling this feature will add a `MetaTagsPart` that can
    be attached to a content item. The plan is for the `Data` property on
    the `MetaTagsPart` to contain the meta content as JSON. When the content
    item is displayed, the JSON will be parsed and rendered in the the
    output via the `IResourceManager`.
    
    Currently the editor experience is a textarea where the user can input
    the JSON. The plan is to add an easy to use UI to manage the meta
    content.

---
+++ MetaTags/Migrations.cs
@@ -0,0 +1,25 @@
+﻿using OrchardCore.ContentManagement.Metadata.Settings;
+using OrchardCore.ContentManagement.Metadata;
+using OrchardCore.Data.Migration;
+
+namespace Moov2.OrchardCore.SEO.MetaTags
+{
+    public class Migrations : DataMigration
+    {
+        IContentDefinitionManager _contentDefinitionManager;
+
+        public Migrations(IContentDefinitionManager contentDefinitionManager)
+        {
+            _contentDefinitionManager = contentDefinitionManager;
+        }
+
+        public int Create()
+        {
+            _contentDefinitionManager.AlterPartDefinition("MetaTagsPart", builder => builder
+                .Attachable()
+                .WithDescription("Provides meta tags for your content item."));
+
+            return 1;
+        }
+    }
+}
