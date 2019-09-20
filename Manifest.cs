3e11cee07587e90fc6590f9d88888e5507455b0c
Lawrence Dine <lawrence.dine@etchuk.com>
2019-09-20 12:16:51 +0100

    Update to bump patch version

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "SEO",
     Author = "Etch",
     Website = "https://etchuk.com",
-    Version = "0.4.3"
+    Version = "0.4.4"
 )]
 
 [assembly: Feature(
