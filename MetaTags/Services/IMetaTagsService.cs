b984b62f39148b0fc58c7c25bd81c4dc9a9fecaf
Peter Keating <peter@moov2.com>
2019-10-16 16:51:24 +0100

    Add ability to specify custom meta tags
    Added a dictionary field (named "Custom") to the `MetaTagsPart` that
    editors can use to define custom meta tags. Any meta tags defined in the
    custom dictionary field will take precedence over the default meta tags
    that get added when specifying an image, title or description.

--- MetaTags/Services/IMetaTagsService.cs
+++ MetaTags/Services/IMetaTagsService.cs
@@ -1,11 +1,14 @@
-﻿using Etch.OrchardCore.SEO.MetaTags.Models;
+﻿using Etch.OrchardCore.Fields.Dictionary.Models;
+using Etch.OrchardCore.SEO.MetaTags.Models;
+using System.Collections.Generic;
 
 namespace Etch.OrchardCore.SEO.MetaTags.Services
 {
     public interface IMetaTagsService
     {
-        void RegisterDefaults(MetaTagsPart metaTags);
-        void RegisterOpenGraph(MetaTagsPart metaTags);
-        void RegisterTwitter(MetaTagsPart metaTags);
+        void RegisterDefaults(MetaTagsPart metaTags, IList<DictionaryItem> customMetaTags = null);
+        void RegisterOpenGraph(MetaTagsPart metaTags, IList<DictionaryItem> customMetaTags = null);
+        void RegisterTwitter(MetaTagsPart metaTags, IList<DictionaryItem> customMetaTags = null);
+        void RegisterCustom(IList<DictionaryItem> customMetaTags);
     }
 }
