34a5e468ab7bcd9134b7b531751a78c875e0a793
Peter Keating <pete.keating@etchuk.com>
2020-05-29 08:45:56 +0100

    Bump to v0.5.5

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "SEO",
     Author = "Etch",
     Website = "https://etchuk.com",
-    Version = "0.5.4"
+    Version = "0.5.5"
 )]
 
 [assembly: Feature(
