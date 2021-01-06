5c6a71de35ea107b5b7106a35c68fdca066a6d3c
Peter Keating <pete.keating@etchuk.com>
2021-01-06 23:28:37 +0000

    Add "Recommended" option to robots.txt modes
    Recommended option will allow all pages except admin pages & login.
    
    Resolves #57.

--- RobotsTxt/Models/RobotsTxtModes.cs
+++ RobotsTxt/Models/RobotsTxtModes.cs
@@ -3,27 +3,32 @@ using System.Collections.Generic;
 
 namespace Etch.OrchardCore.SEO.RobotsTxt.Models
 {
-    public class RobotsTxtModes
+    public static class RobotsTxtModes
     {
         public const int NotDefined = 0;
         public const int AllowAllPages = 1;
         public const int DisallowAllPages = 2;
         public const int Custom = 3;
+        public const int Recommended = 4;
 
-        public static string AllowAllPagesOutput = $"User-agent: *{Environment.NewLine}Disallow:";
-        public static string DisallowAllPagesOutput = $"User-agent: *{Environment.NewLine}Disallow: /";
+        public readonly static string AllowAllPagesOutput = $"User-agent: *{Environment.NewLine}Disallow:";
+        public readonly static string DisallowAllPagesOutput = $"User-agent: *{Environment.NewLine}Disallow: /";
+        public readonly static string RecommendedOutput = $"User-agent: *{Environment.NewLine}Disallow: /login{Environment.NewLine}Disallow: /admin/";
 
         private static IDictionary<int, string> Outputs = new Dictionary<int, string>
         {
             { NotDefined, string.Empty },
             { AllowAllPages, AllowAllPagesOutput},
-            { DisallowAllPages, DisallowAllPagesOutput}
+            { DisallowAllPages, DisallowAllPagesOutput},
+            { Recommended, RecommendedOutput}
         };
 
         public static string GetOutput(RobotsTxtSettings settings)
         {
             if (settings.Mode == Custom)
+            {
                 return settings.CustomContent;
+            }
 
             return Outputs[settings.Mode];
         }
