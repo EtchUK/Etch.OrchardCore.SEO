eb45ee3add5682adaf6ddd2fb5f139098efe76d8
Mehdi Miah <Mehdi.Miah@etchuk.com>
2022-04-28 09:42:25 +0100

    Upgrade to orchard core 1.3.0

--- Manifest.cs
+++ Manifest.cs
@@ -4,7 +4,7 @@ using OrchardCore.Modules.Manifest;
     Name = "SEO",
     Author = "Etch",
     Website = "https://etchuk.com",
-    Version = "1.2.3"
+    Version = "1.3.0"
 )]
 
 [assembly: Feature(
